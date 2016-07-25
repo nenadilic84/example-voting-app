f2b78782f50535bf7e1262c3ffbfb38a1bf9672a
Alexei Ledenev <alexei.ledenev@hp.com>
2016-07-25 16:16:25 +0300

    fix volume remove; retry after sleep

--- clean.sh
+++ clean.sh
@@ -7,8 +7,11 @@ docker service rm $(docker service ls -q)
 docker network rm voteapp
 
 #remove volume; wait some time before remove
-sleep 5
 docker volume rm db-data
+while [ $? -ne 0 ]; do
+  sleep 2
+  docker volume rm db-data
+done
 
 # remove swarm cluster
 docker swarm leave --force
