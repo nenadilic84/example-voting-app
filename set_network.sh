7250cbfc0900cf95c43254f74012554db2eeab26
boriska70@gmail.com <boriska70@gmail.com>
2016-08-04 12:57:59 +0000

    more efficient no_proxy; cleanup

--- set_network.sh
+++ set_network.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 subnet=$(docker network inspect --format='{{(index (index .IPAM.Config) 0).Subnet}}' examplevotingapp_voteapp)
+
 [ $http_proxy ] && tb_proxy=$http_proxy || tb_proxy=$HTTP_PROXY
-[ $no_proxy ] && tb_no_proxy=$no_proxy,result,es,$(seq -s ',' -f $(echo $subnet | cut -d '.' -f1-3).%g 0 255) || tb_no_proxy=$(seq -s ',' -f $(echo $subnet | cut -d '.' -f1-3).%g 0 255)
+tb_no_proxy=$(echo redis,db,voting-app,result,es),$(seq -s ',' -f $(echo $subnet | cut -d '.' -f1-3).%g 0 255)
+[ $no_proxy ] && tb_no_proxy=$tb_no_proxy,$no_proxy,127.0.0.1,localhost || tb_no_proxy=$tb_no_proxy,$NO_PROXY,127.0.0.1,localhost
 
 # echo tb_proxy=$tb_proxy
 # echo tb_no_proxy=$tb_no_proxy
